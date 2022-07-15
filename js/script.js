3287eebcfd2da1b8f792fb8cf13cf9a00797810b
Guilherme Verrange <60721610+Guilherme-Verrange@users.noreply.github.com>
2022-07-15 11:26:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const  menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('header');
+
+
+menuToggle.onclick = function(){
+    navigation.classList.toggle('open')
+   
+}
