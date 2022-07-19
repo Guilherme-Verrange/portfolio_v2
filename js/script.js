ff3469a92c63987476f6b02ecbac7b6792ec823a
Guilherme Verrange <60721610+Guilherme-Verrange@users.noreply.github.com>
2022-07-19 16:07:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,17 +7,3 @@ menuToggle.onclick = function(){
    
 }
 
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-   
-  
-  });
